7c9626a7488640763a48592f798573824457269e
Mike Sannikov <atarity@gmail.com>
2019-04-07 21:21:42 +0400

    echo to file AND tty

--- log-to-file.sh
+++ log-to-file.sh
@@ -9,7 +9,9 @@ do
     # Logging time as UNIX time and temperatures to file
     echo -n "$(date +"%s");" >> $OUT_FILE
     echo -n "$(date --date=@$(date +'%s') '+%Y-%m-%d-%H:%M');" >> $OUT_FILE
-    sensors | sed -n "/+/p" | sed "s/^[a-zA-Z0-9 ]*:\s*\([0-9.+-]*\).*$/\1/" | sed ":a;N;$!ba;s/\n/;\t/g" >> $OUT_FILE
+    #sensors | sed -n "/+/p" | sed "s/^[a-zA-Z0-9 ]*:\s*\([0-9.+-]*\).*$/\1/" | sed ":a;N;$!ba;s/\n/;\t/g" >> $OUT_FILE
+    TEMP="$(sensors | sed -n "/+/p" | sed "s/^[a-zA-Z0-9 ]*:\s*\([0-9.+-]*\).*$/\1/" | sed ':a;N;$!ba;s/\n/;\t/g' | tee /dev/tty);"
+    echo $TEMP >> $OUT_FILE
 
     sleep 3
 done
